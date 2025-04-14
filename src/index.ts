0e0010690188746854c94bb631e18b93416980eb
booky10 <boooky10@gmail.com>
2025-04-15 00:45:11 +0200

    Finish initial worker implementation

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
+const defaultHandler = {
     async fetch(request, env, ctx): Promise<Response> {
         const url = new URL(request.url);
         switch (url.pathname) {
             case '/':
                 url.pathname = 'home.html';
                 return env.ASSETS.fetch(url);
+            case '/instances.json':
+            case '/uptime':
+                // TODO is it possible to track uptime in a serverless environment?
+                return fetch(`https://jank.booky.dev${url.pathname}`);
             default:
+                // special invite SPA
+                if (url.pathname.startsWith('/invite/')) {
+                    url.pathname = '/invite.html';
+                }
                 return env.ASSETS.fetch(request);
         }
+    }
+} satisfies ExportedHandler<Env>;
+
+export default {
+    async fetch(request, env, ctx): Promise<Response> {
+        // fetch normal response
+        const resp = await defaultHandler.fetch(request, env, ctx);
+        // build oembed link string and inject into a new set of headers
+        const oembedLink = new URL('/services/oembed', request.url);
+        oembedLink.searchParams.set('url', request.url);
+        const newHeaders = new Headers(resp.headers);
+        newHeaders.set('Link', `<${oembedLink.toString()}>; rel="alternate"; type="application/json+oembed"; title="Jank Client oEmbed format"`);
+        // re-build response object
+        return new Response(resp.body, {
+            status: resp.status,
+            statusText: resp.statusText,
+            headers: newHeaders
+        });
     }
 } satisfies ExportedHandler<Env>;
